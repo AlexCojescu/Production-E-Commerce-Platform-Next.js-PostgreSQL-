3a05797a85a881635b8433dfd545abc6da6d605e
alexcojescu <alexcojescu@gmail.com>
2025-11-11 19:21:50 -0800

    fourth (again)

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -7,11 +7,8 @@ from "@/inngest/functions";
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-   
     syncUserCreation,
     syncUserUpdation,
     syncUserDeletion
-
-
   ],
 });
